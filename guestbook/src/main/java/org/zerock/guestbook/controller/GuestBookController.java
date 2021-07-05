381d1cc987a707affc02af74f98a89c0c45d0f24
Duhanmo <devduhanmo@gmail.com>
2021-07-05 18:10:08 +0900

    방명록 만들기: 모달이 안뜸

--- guestbook/src/main/java/org/zerock/guestbook/controller/GuestBookController.java
+++ guestbook/src/main/java/org/zerock/guestbook/controller/GuestBookController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook.dto.GuestbookDTO;
 import org.zerock.guestbook.dto.PageRequestDTO;
 import org.zerock.guestbook.service.GuestbookService;
 
@@ -30,5 +33,21 @@ public class GuestBookController {
         log.info("list............." + pageRequestDTO);
 
         model.addAttribute("result", guestbookService.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get.........");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto..." + dto);
+
+        Long id = guestbookService.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", id);
+
+        return "redirect:/guestbook/list";
     }
 }
